4645bf4f5ce7b012f35d2f63d91e6f0de86ac60b
user <user@local>
2025-04-11 18:23:22 -0700

    Upload Weather App

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-orange-500 grid grid-cols-3 sm:grid-cols-3 gap-4">
+		{/* Flexible rectangle cards */}
+		<div className="rounded-xl text-gray-300 m-4 h-48 bg-gray-900"></div>
+	    <div className="rounded-xl text-gray-300 m-4 h-48 bg-gray-900">
+	    <div className="pl-4 text-left">Reykj</div>
+	    </div>
+	    <div className="rounded-xl text-gray-300 m-4 h-48 bg-gray-900"></div>
+	  </div>
+      <h1 className="bg-gray-700 h-64 text-white text-3xl border border-gray-600 flex items-center justify-center">PARTLY CLOUDY - 42 FAHR. - REYKJAVÍK</h1>
+      <h6 className="bg-white h-16 text-xl border">Tailwind - React - Typescript</h6>
+      <div className="bg-white text-left pl-2"><p>BtnUp</p></div>
+      <div className="bg-white text-left pl-2"><p>Portfolio - o</p></div>
+    </>
+  )
+}
+
+export default App
