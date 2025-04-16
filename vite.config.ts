c106068957969d6ca097ef2e0b774649883e15e0
user <ianforrestlee@gmail.com>
2025-04-16 09:18:42 -0700

    More Vercel Debugging

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
+  /*
     'import.meta.env': process.env,
-    'process.env' : process.env
+    'process.env' : process.env,
+    'VITE_OPENWEATHERMAP_KEY': import.meta.env.VITE_OPENWEATHERMAP_KEY,
+  */
   }
 })
